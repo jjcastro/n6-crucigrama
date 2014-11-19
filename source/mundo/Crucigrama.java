db7f87a159580e97eafb4f952c8b52f218a52d63
jj.castro10 <jj.castro10@isc231.uniandes.edu.co>
2014-11-19 08:56:14 -0500

    Añadir método loadProperties()

--- source/mundo/Crucigrama.java
+++ source/mundo/Crucigrama.java
@@ -1,5 +1,25 @@
 package mundo;
 
+import java.io.FileInputStream;
+import java.io.File;
+import java.util.Properties;
+
 public class Crucigrama {
 
+	public Properties loadProperties(File archivo) throws Exception
+	{
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(archivo);
+		try
+		{
+			prop.load(fis);
+		}
+		catch (Exception e)
+		{
+			throw new Exception("Formato inv‡lido");
+		}
+		fis.close();
+		return prop;
+	}
+	
 }
