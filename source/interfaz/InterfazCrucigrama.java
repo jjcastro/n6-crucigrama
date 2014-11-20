e63e247c81c942065af1d2ce7729dd7b9556228a
josecastrovaron <jj.castro10@uniandes.edu.co>
2014-11-20 08:51:20 -0500

    Crear constructor con un banner y BorderLayout

--- source/interfaz/InterfazCrucigrama.java
+++ source/interfaz/InterfazCrucigrama.java
@@ -1,11 +1,27 @@
 package interfaz;
 
+import java.awt.BorderLayout;
+
 import javax.swing.*;
+
+import mundo.Crucigrama;
 
 public class InterfazCrucigrama extends JFrame
 {
 	private PanelAcciones pnlAcciones;
 	private PanelPrincipal pnlPrincipal;
 	
+	private Crucigrama crucigrama;
 	
+	public InterfazCrucigrama()
+	{
+		setLayout(new BorderLayout());
+		
+		JLabel banner = new JLabel("./data/imagenes/banner.jpg");
+		add(banner, BorderLayout.NORTH);
+		
+		pack();
+		setVisible(true);
+		
+	}
 }
