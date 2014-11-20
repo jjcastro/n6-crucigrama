0ec983fa79088ddaaa7c6b318c8ad3197f6cb253
josecastrovaron <jj.castro10@uniandes.edu.co>
2014-11-20 09:40:19 -0500

    Añadir los paneles del centro y configurarlos

--- source/interfaz/PanelPrincipal.java
+++ source/interfaz/PanelPrincipal.java
@@ -3,12 +3,16 @@ package interfaz;
 import java.awt.GridLayout;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 
 public class PanelPrincipal extends JPanel
 {
 	private InterfazCrucigrama interfaz;
 	
 	private JTextField[][] casillas;
+	
+	private JTextArea txtDescripcionesH;
+	private JTextArea txtDescripcionesV;
 	
 	public PanelPrincipal(int filas, int columnas, InterfazCrucigrama pInterfaz)
 	{
@@ -19,7 +23,18 @@ public class PanelPrincipal extends JPanel
 		JPanel pnlCrucigrama = new JPanel();
 		
 		JPanel pnlDescripciones = new JPanel();
+		pnlDescripciones.setLayout(new GridLayout(2,1));
 		
+		txtDescripcionesH = new JTextArea();
+		txtDescripcionesH.setBorder(new TitledBorder("Horizontales"));
 		
+		txtDescripcionesV = new JTextArea();
+		txtDescripcionesH.setBorder(new TitledBorder("Verticales"));
+		
+		pnlDescripciones.add(txtDescripcionesH);
+		pnlDescripciones.add(txtDescripcionesV);
+		
+		add(pnlCrucigrama);
+		add(pnlDescripciones);
 	}
 }
